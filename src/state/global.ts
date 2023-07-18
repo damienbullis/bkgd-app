eadec772c28ffca9d74244f25ab1f3dffe432f22
Damien <damien@bodhi.digital>
2023-07-18 08:52:55 -0500

    code split

--- src/state/global.ts
+++ src/state/global.ts
@@ -1,44 +1,10 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
 import SubStore from './subStore'
 
-const ActiveLayerID = new SubStore('')
-
 const Visible = new SubStore(true)
-
-const Layer = {
-  ActiveLayerID,
-  Visible,
-} as const
-
-type Layer = typeof Layer
-type LayerStore<T extends keyof Layer> = Layer[T]
-type ValueType<T> = T extends SubStore<infer Value> ? Value : never
-
-const useLayer = <T extends keyof Layer, V extends ValueType<LayerStore<T>>>(
-  selector: T
-) => {
-  const store = useMemo(
-    () => Layer[selector] as unknown as SubStore<V>,
-    [selector]
-  )
-
-  const [_state, setState] = useState(store.value())
-
-  useEffect(() => {
-    const [unsubscribe] = store.subscribe((state) => setState(state))
-    return () => unsubscribe()
-  }, [selector, store])
-
-  const setter = useCallback<(data: V) => void>(
-    (state) => store.publish(state),
-    [store]
-  )
-
-  return [_state, setter] as const
-}
 
 const UI = {
   Visible,
 }
 
-export { Layer, UI, useLayer }
+export { UI }
+export { Layer, useLayer } from './Layer'
