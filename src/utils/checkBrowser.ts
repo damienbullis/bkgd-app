59d29b09f923cc42a353f755f3c0deafe651437d
Damien <80228708+damienbullis@users.noreply.github.com>
2023-07-29 17:35:36 -0500

    moves checkBrowers to utils

---
+++ src/utils/checkBrowser.ts
@@ -0,0 +1,25 @@
+/** @throws {Error} - If browser is not Chrome */
+export default function checkBrowser() {
+  // Get the user-agent string
+  const userAgentString = navigator.userAgent
+
+  // Detect Chrome
+  let chromeAgent = userAgentString.indexOf('Chrome') > -1
+
+  // Detect Safari
+  let safariAgent = userAgentString.indexOf('Safari') > -1
+
+  // Discard Safari since it also matches Chrome
+  if (chromeAgent && safariAgent) safariAgent = false
+
+  // Detect Opera
+  const operaAgent = userAgentString.indexOf('OP') > -1
+
+  // Discard Chrome since it also matches Opera
+  if (chromeAgent && operaAgent) chromeAgent = false
+
+  // TODO: do we care about this?
+  // if we are doing serverless screenshots,
+  // we might not need to care about this
+  if (!chromeAgent) throw new Error('Chrome is required')
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ export { default as throttle } from './throttle'
 export { default as debounce } from './debounce'
 export { hexToHSL, hexToRGB } from './colorHelpers'
 export { default as calcAverageColor } from './calcAverageColor'
+export { default as checkBrowser } from './checkBrowser'
 
 export function makeID() {
   return Math.random().toString(36).substring(2, 7)
