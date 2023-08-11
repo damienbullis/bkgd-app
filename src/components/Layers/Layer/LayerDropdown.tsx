c5411044b2127051c042a22d73c8d7a65fb906d1
Damien <80228708+damienbullis@users.noreply.github.com>
2023-08-10 20:26:18 -0500

    more updates to layer button styles

--- src/components/Layers/Layer/LayerDropdown.tsx
+++ src/components/Layers/Layer/LayerDropdown.tsx
@@ -7,24 +7,37 @@ import {
 import IconButton from '../../_shared/IconButton'
 import { EventHandler } from '@state/events'
 import styles from './_.module.css'
+import { useEffect, useState } from 'react'
 
 const LayerDropdown = ({ id, isActive }: { id: string; isActive: boolean }) => {
+  const [menu, setMenu] = useState(false)
+  const showEllipse = isActive ? styles.active : ''
+  const showMenu = menu ? styles.active : ''
+
+  const prevent =
+    (fn: () => void) => (e: React.MouseEvent<HTMLButtonElement>) => {
+      console.log('prevent')
+      e.preventDefault()
+      e.stopPropagation()
+      fn()
+    }
+  useEffect(() => {
+    if (!isActive) setMenu(false)
+  }, [isActive])
   return (
     <div className={styles.ellipse}>
       <IconButton
         icon={DotsThreeOutlineVertical}
         size="sm"
-        className={isActive ? styles.active : ''}
-        onClick={(e) => {
-          e.preventDefault()
-          e.stopPropagation()
-        }}
+        disabled={!isActive}
+        className={showEllipse}
+        onClick={prevent(() => setMenu(!menu))}
       />
-      <div className={styles.dropdown}>
+      <div className={`${styles.dropdown} ${showMenu}`}>
         <IconButton
           icon={ArrowBendLeftUp}
           size="sm"
-          onClick={() =>
+          onClick={prevent(() =>
             EventHandler({
               action: 'bkgd-update-stack',
               payload: {
@@ -32,12 +45,12 @@ const LayerDropdown = ({ id, isActive }: { id: string; isActive: boolean }) => {
                 direction: 'up',
               },
             })
-          }
+          )}
         />
         <IconButton
           icon={ArrowBendRightDown}
           size="sm"
-          onClick={() =>
+          onClick={prevent(() =>
             EventHandler({
               action: 'bkgd-update-stack',
               payload: {
@@ -45,17 +58,17 @@ const LayerDropdown = ({ id, isActive }: { id: string; isActive: boolean }) => {
                 direction: 'down',
               },
             })
-          }
+          )}
         />
         <IconButton
           icon={TrashSimple}
           size="sm"
-          onClick={() =>
+          onClick={prevent(() =>
             EventHandler({
               action: 'bkgd-remove-layer',
               payload: { id },
             })
-          }
+          )}
         />
       </div>
     </div>
