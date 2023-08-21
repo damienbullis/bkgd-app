8280513ce9aea485d7d1d403b763afc4b9904b48
Damien <80228708+damienbullis@users.noreply.github.com>
2023-08-20 22:37:57 -0500

    render almost working for basic gradient

--- src/components/Bkgd/LayerTypes/GradientLayer.tsx
+++ src/components/Bkgd/LayerTypes/GradientLayer.tsx
@@ -1,3 +1,4 @@
+import { hexToRGB } from '../../../utils/colorHelpers'
 import {
   GradientLayerType,
   SharedLayerPropsSchemaType,
@@ -5,9 +6,37 @@ import {
 
 type GradientLayerProps = GradientLayerType & SharedLayerPropsSchemaType
 
+const getColor = (
+  color:
+    | string
+    | { r: number; g: number; b: number }
+    | { h: number; s: number; l: number },
+  opacity: number | null
+) => {
+  if (typeof color === 'string') {
+    // reduce to rgba()
+    const { r, g, b } = hexToRGB(color)
+    return `rgba(${r}, ${g}, ${b}, ${opacity ?? 1})`
+  }
+}
+
 const GradientLayer = (layer: GradientLayerProps) => {
   console.log('GradientLayer', { layer })
-  return ''
+  if (layer.props.type === 'linear') {
+    return `linear-gradient(${layer.props.deg || 0}deg in ${
+      layer.props.colorSpace || 'oklab'
+    }, ${layer.props.stops?.map(([color, opacity, stop]) => {
+      return `${getColor(color, opacity)} ${
+        stop
+          ? `${
+              typeof stop === 'number'
+                ? `${stop}%`
+                : stop.map((s) => s + '%').join(' ')
+            }`
+          : ''
+      }`
+    })})`
+  }
 }
 
 export default GradientLayer
