0df7c0f37e242ff65bd40f731650af335c9acb70
Damien <80228708+damienbullis@users.noreply.github.com>
2023-09-10 20:06:45 -0500

    adds submenu to helpmenu component

--- src/components/Nav/HelpMenu.tsx
+++ src/components/Nav/HelpMenu.tsx
@@ -8,6 +8,42 @@ import {
   Question,
 } from '@phosphor-icons/react'
 
+const SubMenu = () => {
+  return (
+    <Menu>
+      <Menu.Button
+        onClick={(e) => e.stopPropagation()}
+        className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap p-1 px-4 text-sm font-normal ui-active:bg-slate-200 ui-active:text-slate-950"
+      >
+        Export Size
+        <span className="ml-auto text-lg">
+          <ArrowsOutSimple />
+        </span>
+      </Menu.Button>
+      <Transition
+        className="absolute bottom-0 right-full z-20 mr-2 origin-bottom-right rounded-lg bg-slate-200 shadow-[0_8px_20px_0_#00000040]"
+        enter="transition duration-100 ease-out"
+        enterFrom="transform scale-0 opacity-0 translate-x-full"
+        enterTo="transform scale-100 opacity-100 translate-x-0"
+        leave="transition duration-75 ease-out"
+        leaveFrom="transform scale-100 opacity-100 translate-x-0"
+        leaveTo="transform scale-0 opacity-0 translate-x-full"
+      >
+        <Menu.Items className="py-3">
+          <p className="inline-flex min-w-[10rem] max-w-[16rem] flex-nowrap p-1 px-4 text-sm font-light text-slate-950 ">
+            More Coming Soon...
+          </p>
+          <Menu.Item>
+            <p className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap p-1 px-4 text-sm font-light text-slate-950 hover:bg-slate-300">
+              1600 x 900
+            </p>
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  )
+}
+
 export default function HelpMenu() {
   return (
     <div className="relative mx-auto flex py-4">
@@ -37,29 +73,33 @@ export default function HelpMenu() {
               </p>
             </Menu.Item>
             <Menu.Item>
-              <p className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap p-1 px-4 text-sm font-normal ui-active:bg-slate-200 ui-active:text-slate-950">
-                Export Size
-                <span className="ml-auto text-lg">
-                  <ArrowsOutSimple />
-                </span>
-              </p>
+              <div className="relative">
+                <SubMenu />
+              </div>
             </Menu.Item>
             <Menu.Item>
-              <p className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap items-center p-1 px-4 text-sm font-normal ui-active:bg-slate-200 ui-active:text-slate-950">
+              <a
+                href="https://github.com/damienbullis/bkgd-app/issues"
+                className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap items-center p-1 px-4 text-sm font-normal ui-active:bg-slate-200 ui-active:text-slate-950"
+              >
                 Report Issues
                 <span className="ml-auto text-lg">
                   <Bug />
                 </span>
-              </p>
+              </a>
             </Menu.Item>
             <p className="my-2 mt-3 h-[1px] w-full bg-white bg-opacity-20"></p>
             <Menu.Item>
-              <p className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap p-1 px-4 text-sm font-normal ui-active:bg-slate-200 ui-active:text-slate-950">
+              <a
+                href="https://github.com/damienbullis/bkgd-app"
+                target="_blank"
+                className="inline-flex min-w-[10rem] max-w-[16rem] cursor-pointer flex-nowrap p-1 px-4 text-sm font-normal ui-active:bg-slate-200 ui-active:text-slate-950"
+              >
                 Gitub
                 <span className="ml-auto text-lg">
                   <GitBranch />
                 </span>
-              </p>
+              </a>
             </Menu.Item>
           </Menu.Items>
         </Transition>
