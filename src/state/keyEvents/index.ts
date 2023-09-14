d17cd933adf961224aba178d1b702919b4f40f4e
Damien <80228708+damienbullis@users.noreply.github.com>
2023-09-14 17:21:06 -0500

    creates a pubsub context for keyevents

--- src/state/keyEvents/index.ts
+++ src/state/keyEvents/index.ts
@@ -1,18 +1,73 @@
-export function setupKeyboardEvents(document: Document) {
-  console.log(setupKeyboardEvents.name)
-  document.addEventListener('keydown', (event) => {
-    console.log(event)
+import { createContext } from 'react'
 
-    // FEATURE: add keyboard shortcuts
-    // FEATURE: add event modifiers
-
-    if (event.key === 'Escape') console.log('Escape')
-    if (event.key === 'Enter') console.log('Enter')
-    if (event.key === 'ArrowUp') console.log('ArrowUp')
-    if (event.key === 'ArrowDown') console.log('ArrowDown')
-    if (event.altKey) console.log('altKey')
-    if (event.ctrlKey) console.log('ctrlKey')
-    if (event.metaKey) console.log('metaKey')
-    if (event.shiftKey) console.log('shiftKey')
-  })
+type KeyEventsState = {
+  shiftKey: boolean
+  ctrlKey: boolean
+  altKey: boolean
+  key: string
 }
+class KeyboardEvents {
+  private state: KeyEventsState
+  private subscribers: ((d: KeyEventsState) => void)[] = []
+  constructor() {
+    this.state = {
+      shiftKey: false,
+      ctrlKey: false,
+      altKey: false,
+      key: '',
+    }
+    this.setupKeyboardEvents()
+  }
+
+  public subscribe(callback: (d: KeyEventsState) => void) {
+    this.subscribers.push(callback)
+    return this.unsubscribe.bind(this, callback)
+  }
+
+  private unsubscribe(callback: (d: KeyEventsState) => void) {
+    this.subscribers = this.subscribers.filter(
+      (subscriber) => subscriber !== callback
+    )
+  }
+
+  private publish() {
+    for (const callback of this.subscribers) {
+      callback(this.state)
+    }
+  }
+
+  private setupKeyboardEvents() {
+    document.addEventListener('keydown', (event) => {
+      if (event.altKey) {
+        this.state.altKey = true
+      } else if (event.ctrlKey) {
+        this.state.ctrlKey = true
+      } else if (event.metaKey) {
+        this.state.ctrlKey = true
+      } else if (event.shiftKey) {
+        this.state.shiftKey = true
+      }
+      this.state.key = event.key
+      this.publish()
+    })
+
+    document.addEventListener('keyup', (event) => {
+      if (event.altKey) {
+        this.state.altKey = false
+      } else if (event.ctrlKey) {
+        this.state.ctrlKey = false
+      } else if (event.metaKey) {
+        this.state.ctrlKey = false
+      } else if (event.shiftKey) {
+        this.state.shiftKey = false
+      }
+      this.state.key = ''
+      this.publish()
+    })
+  }
+}
+
+const KeyEventsContext = createContext<KeyboardEvents>(new KeyboardEvents())
+const KeyEventsProvider = KeyEventsContext.Provider
+
+export { KeyEventsContext, KeyEventsProvider, KeyboardEvents }
