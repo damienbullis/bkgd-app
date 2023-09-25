bbcf0a73186977cae071ba05488bf1a5af7b4e33
Damien <80228708+damienbullis@users.noreply.github.com>
2023-09-25 08:44:16 -0500

    hmmmm

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+export default {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
