c27ac53428e4e5498d7dab13ff533dfa8a3bba58
Damien <80228708+damienbullis@users.noreply.github.com>
2023-10-12 19:08:08 -0500

    move @font-face to index.html

---
+++ .changeset/odd-horses-ring.md
@@ -0,0 +1,5 @@
+---
+'bkgd-app': patch
+---
+
+another attempt to get fonts working
--- src/components/Pending.tsx
+++ src/components/Pending.tsx
@@ -1,3 +1,9 @@
+import { Spiral } from '@phosphor-icons/react'
+
 export default function Pending() {
-  return <div className="fixed inset-0 grid place-items-center">Loading...</div>
+  return (
+    <div className="fixed inset-0 grid place-items-center">
+      <Spiral className="animate-spin text-4xl text-white" />
+    </div>
+  )
 }
